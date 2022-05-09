7f88efc8cd41a91b595b28f9d8ba843dfdc82d85
MonaMahmoud <monamohamedfahmy@gmail.com>
2022-05-09 21:03:52 +1000

    updated index.js in models folder

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,9 @@
 const User = require('./User');
 const Thought = require('./Task');
+const Category = require('./Category');
+const SubCategory = require('./SubCategory');
+const Frequency = require('./Frequency');
+const Child = require('./Child');
 
-module.exports = { User, Thought };
+
+module.exports = { User, Thought, Category, SubCategory, Frequency, Child };
